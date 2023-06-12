fc9e34062bedfddd47a8f54819e4e7f6cefab7c9
Andrew Van De Poel <andrewvdp@gmail.com>
2023-06-12 12:48:13 -0700

    spelling error

--- shell/bridge.deploy.sh
+++ shell/bridge.deploy.sh
@@ -13,12 +13,24 @@ Host xmtp-bridge-prod
   StrictHostKeyChecking no
 END
 
-if [ "${BOOT_MODE}" == "network" ]; then
+bridge_replica_count=$(DOCKER_HOST=ssh://xmtp-bridge-prod docker compose -f ./docker/docker-compose.prod.yml ps | grep bridge | wc -l)
+
+if [ "$BOOT_MODE" = "network" ]; then
   echo "Booting from config file"
   echo "${BOOT_CONFIG_B64}" | base64 --decode > ./shell/boot.json
+
+  scp ./shell/boot.json xmtp-bridge-prod:/root/app
+
+  boot_config=$(cat ./shell/boot.json | jq -c '.')
+  boot_config_length=$(echo $boot_config | jq '. | length')
+  scale_number=$((bridge_replica_count+boot_config_length))
+else
+  echo "Booting from env var"
+  scale_number=$((bridge_replica_count+1))
 fi
 
-cat ~/.ssh/config
 
-echo "Deploying bridge"
-DOCKER_HOST=ssh://xmtp-bridge-prod docker compose -f ./docker/docker-compose.yml up -d
+cd nodejs
+
+echo "Deploying bridge to $scale_number replicas"
+npm run prod:up -- bridge=$scale_number
