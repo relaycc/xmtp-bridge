6d108b5fd0231f08c06fcd853013da17d4323751
Andrew Van De Poel <andrewvdp@gmail.com>
2023-06-22 13:22:45 -0700

    docs for deployments

--- shell/bridge.deploy.sh
+++ shell/bridge.deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-. ./shell/env.sh
-
 mkdir -p ~/.ssh
 echo "${XMTP_BRIDGE_DROPLET_PK}" > ~/.ssh/xmtp-bridge-prod
 chmod 400 ~/.ssh/xmtp-bridge-prod
@@ -13,24 +11,32 @@ Host xmtp-bridge-prod
   StrictHostKeyChecking no
 END
 
-bridge_replica_count=$(DOCKER_HOST=ssh://xmtp-bridge-prod docker compose -f ./docker/docker-compose.prod.yml ps | grep bridge | wc -l)
 
-if [ "$BOOT_MODE" = "network" ]; then
+if [ ! -z $BOOT_CONFIG_B64 ]; then
+  export _BOOT_MODE="network"
+  . ./shell/env.sh
   echo "Booting from config file"
   echo "${BOOT_CONFIG_B64}" | base64 --decode > ./shell/boot.json
 
   scp ./shell/boot.json xmtp-bridge-prod:/root/app
 
   boot_config=$(cat ./shell/boot.json | jq -c '.')
-  boot_config_length=$(echo $boot_config | jq '. | length')
-  scale_number=$((bridge_replica_count+boot_config_length))
-else
+  scale_number=$(echo $boot_config | jq '. | length')
+
+  cd nodejs
+  echo "Deploying bridge to $scale_number replicas"
+  npm run prod:reset -- bridge=$scale_number
+elif [ ! -z $_KEY_TO_BOOT ]; then
   echo "Booting from env var"
+  export _BOOT_MODE="env"
+  . ./shell/env.sh
+  bridge_replica_count=$(DOCKER_HOST=ssh://xmtp-bridge-prod docker compose -f ./docker/docker-compose.prod.yml ps | grep bridge | wc -l)
   scale_number=$((bridge_replica_count+1))
+
+  cd nodejs
+  echo "Deploying bridge to $scale_number replicas"
+  npm run prod:add -- bridge=$scale_number
+else
+  echo "No boot config found"
+  exit 1
 fi
-
-
-cd nodejs
-
-echo "Deploying bridge to $scale_number replicas"
-npm run prod:up -- bridge=$scale_number
