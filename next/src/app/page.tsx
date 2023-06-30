074a1e5fd35b60b32e087f6a3c5ec078e9d2703c
achilles schmelzer <achilles@daopanel.com>
2023-06-30 12:58:51 -0700

    Add frontend skeleton

---
+++ next/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return <main className="w-full h-screen flex flex-col justify-center items-center">
+    <h1 className="text-4xl font-bold">XMTP BRIDGE</h1>
+  </main>
+}
