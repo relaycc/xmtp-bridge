248cbc56de42fa39b64e7472fd446f77f9129194
achilles schmelzer <achilles@daopanel.com>
2023-07-04 11:12:54 -0700

    Add warnings to env.sh

--- shell/env.sh
+++ shell/env.sh
@@ -5,6 +5,10 @@
 #
 #
 
+if [ -z "${_XMTPB_SENTRY_DSN}" ]; then
+  echo "WARNING :: XMTPB_SENTRY_DSN is not set"
+fi
+
 export XMTPB_SENTRY_DSN=${_XMTPB_SENTRY_DSN}
 
 
@@ -13,12 +17,20 @@ export XMTPB_SENTRY_DSN=${_XMTPB_SENTRY_DSN}
 #
 #
 
+if [ -z "${_XMTPB_API_URL}" ]; then
+  echo "WARNING :: XMTPB_API_URL is not set"
+fi
+
 export XMTPB_API_URL=${_XMTPB_API_URL}
 
 #
 # API_PORT is the port of the cache service.
 #
 #
+
+if [ -z "${_XMTPB_API_PORT}" ]; then
+  echo "WARNING :: XMTPB_API_PORT is not set"
+fi
 
 export XMTPB_API_PORT=${_XMTPB_API_PORT}
 
@@ -27,12 +39,20 @@ export XMTPB_API_PORT=${_XMTPB_API_PORT}
 # messages to bridge instances.
 #
 
+if [ -z "${_XMTPB_WEBHOOK_KEY}" ]; then
+  echo "WARNING :: XMTPB_WEBHOOK_KEY is not set"
+fi
+
 export XMTPB_WEBHOOK_KEY=${_XMTPB_WEBHOOK_KEY}
 
 #
 # The connection string for the project's postgres database.
 #
 #
+
+if [ -z "${_XMTPB_PG_CONNECTION_STRING}" ]; then
+  echo "WARNING :: XMTPB_PG_CONNECTION_STRING is not set"
+fi
 
 export XMTPB_PG_CONNECTION_STRING=${_XMTPB_PG_CONNECTION_STRING}
 
@@ -41,12 +61,20 @@ export XMTPB_PG_CONNECTION_STRING=${_XMTPB_PG_CONNECTION_STRING}
 #
 #
 
+if [ -z "${_XMTPB_SIGNUP_KEY}" ]; then
+  echo "WARNING :: XMTPB_SIGNUP_KEY is not set"
+fi
+
 export XMTPB_SIGNUP_KEY=${_XMTPB_SIGNUP_KEY}
 
 #
 # The address of the XMTP identity we want to bridge.
 #
 #
+
+if [ -z "${_XMTPB_BRIDGE_ADDRESS}" ]; then
+  echo "WARNING :: XMTPB_BRIDGE_ADDRESS is not set"
+fi
 
 export XMTPB_BRIDGE_ADDRESS=${_XMTPB_BRIDGE_ADDRESS}
 
@@ -55,12 +83,20 @@ export XMTPB_BRIDGE_ADDRESS=${_XMTPB_BRIDGE_ADDRESS}
 #
 #
 
+if [ -z "${_XMTPB_VIRTUAL_HOST}" ]; then
+  echo "WARNING :: XMTPB_VIRTUAL_HOST is not set"
+fi
+
 export XMTPB_VIRTUAL_HOST=${_XMTPB_VIRTUAL_HOST}
 
 #
 # LETSENCRYPT_HOST is the domain name that the bridge will be hosted on.
 #
 #
+
+if [ -z "${_XMTPB_LETSENCRYPT_HOST}" ]; then
+  echo "WARNING :: XMTPB_LETSENCRYPT_HOST is not set"
+fi
 
 export XMTPB_LETSENCRYPT_HOST=${_XMTPB_LETSENCRYPT_HOST}
 
@@ -69,12 +105,20 @@ export XMTPB_LETSENCRYPT_HOST=${_XMTPB_LETSENCRYPT_HOST}
 #
 #
 
+if [ -z "${_XMTPB_SSH_HOST}" ]; then
+  echo "WARNING :: XMTPB_SSH_HOST is not set"
+fi
+
 export XMTPB_SSH_HOST=${_XMTPB_SSH_HOST}
 
 #
 # XMTPB_DEFAULT_EMAIL is the email LetsEncrypt will use to contact us about our
 # SSL certificates.
 #
+
+if [ -z "${_XMTPB_DEFAULT_EMAIL}" ]; then
+  echo "WARNING :: XMTPB_DEFAULT_EMAIL is not set"
+fi
 
 export XMTPB_DEFAULT_EMAIL=${_XMTPB_DEFAULT_EMAIL}
 
@@ -83,6 +127,10 @@ export XMTPB_DEFAULT_EMAIL=${_XMTPB_DEFAULT_EMAIL}
 #
 #
 
+if [ -z "${_XMTPB_IMAGE_TAG}" ]; then
+  echo "WARNING :: XMTPB_IMAGE_TAG is not set"
+fi
+
 export XMTPB_IMAGE_TAG=${_XMTPB_IMAGE_TAG}
 
 #
@@ -90,11 +138,19 @@ export XMTPB_IMAGE_TAG=${_XMTPB_IMAGE_TAG}
 # Only used in test.
 #
 
+if [ -z "${_XMTPB_POSTGRES_USER}" ]; then
+  echo "WARNING :: XMTPB_POSTGRES_USER is not set"
+fi
+
 export XMTPB_POSTGRES_USER=${_XMTPB_POSTGRES_USER}
 
 #
 # XMTPB_POSTGRES_PASSWORD is the postgres password to use for the postgres
 # database. Only used in test.
 #
+
+if [ -z "${_XMTPB_POSTGRES_PASSWORD}" ]; then
+  echo "WARNING :: XMTPB_POSTGRES_PASSWORD is not set"
+fi
 
 export XMTPB_POSTGRES_PASSWORD=${_XMTPB_POSTGRES_PASSWORD}
